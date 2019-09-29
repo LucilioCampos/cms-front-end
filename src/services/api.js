9e0642a792b7ae67578c71c2049e6a68305804cf
lucilio <lucilio@codeby.com.br>
2019-09-29 16:02:55 -0300

    add env production baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import { getToken } from './auth'
 export const api = axios.create({ baseURL: 'https://rocketseat-node.herokuapp.com/api' })
 
 export const conn = axios.create({
-  baseURL: "http://localhost:3000/api/v1"
+  baseURL: process.env.production ? "https://sales-cms-backend.herokuapp.com/api/v1" : "http://localhost:3000/api/v1"
 })
 
 conn.interceptors.request.use(async config => {
